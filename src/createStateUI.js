c90fa899831c6215f7ee535471f366ebb47b0e4c
Darnell Gillead <dlgillead@gmail.com>
2017-08-08 18:22:11 -0500

    complete

--- src/createStateUI.js
+++ src/createStateUI.js
@@ -5,8 +5,13 @@ class CreateStates extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      stateNames : this.getStateNames()
+      stateNames: this.getStateNames(),
+      abbreviation: '',
+      selectedState: 'Alabama'
     }
+
+    this.getAbbreviation = this.getAbbreviation.bind(this);
+    this.updateSelectedState = this.updateSelectedState.bind(this);
   }
 
   getStateNames() {
@@ -17,18 +22,39 @@ class CreateStates extends Component {
     return stateNames;
   }
 
+  getAbbreviation(e) {
+    e.preventDefault();
+    let selectedState = this.state.selectedState;
+    let currentState = stateData.filter(function(state) {
+      return (state.name === selectedState)
+    });
+    let abbr = currentState[0]['alpha-2'];
+    this.setState({
+      abbreviation : abbr
+    });
+  }
+
   fillDropDown(stateName) {
-    return <option key={stateName}>{stateName}</option>
+    return <option value={stateName} key={stateName}>{stateName}</option>
+  }
+
+  updateSelectedState(e) {
+    this.setState({selectedState: e.target.value});
   }
 
   render() {
     return (
-      <select>
-        {this.state.stateNames.map(this.fillDropDown)}
-      </select>
+      <div>
+        <form>
+          <select onChange={this.updateSelectedState}>
+            {this.state.stateNames.map(this.fillDropDown)}
+          </select>
+          <input type="submit" value="See Abbreviation" onClick={this.getAbbreviation}></input>
+          <p>{this.state.abbreviation}</p>
+        </form>
+      </div>
     )
   }
-
 }
 
 export default CreateStates;
